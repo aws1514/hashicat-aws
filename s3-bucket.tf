e38b1f206b38f4c8bc519ed3d89eb9a4ff295e4b
Aws Christ <awschrist@gmail.com>
2024-10-20 13:47:40 +0000

    Added S3 bucket module for Gaurav

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,4 @@
 
-variable "prefix" {
-  description = "Prefix for the S3 bucket name"
-  default     = "gaurav"
-}
-
 module "s3-bucket" {
   source  = "app.terraform.io/aws1514/s3-bucket/aws"
   version = "2.8.0"
